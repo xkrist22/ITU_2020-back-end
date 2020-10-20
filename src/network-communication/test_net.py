b0b2038bff7130458ae5afa0526eb8241724aff1
poli-cz <61143280+poli-cz@users.noreply.github.com>
2020-10-20 19:55:49 +0200

    Some comments

--- src/network-communication/test_net.py
+++ src/network-communication/test_net.py
@@ -6,16 +6,32 @@ import time
 
 
 Server = server() # create server
-ip = Server.get_ip()
+ip = Server.get_ip() # get server ip
 
-client1 = client(ip, "test")
+
+
+client1 = client(ip, "test") # client require ip of server and Uniqe ID
 client2 = client(ip, "test2")
 client3 = client(ip, "test3")
 
-
-client1.send("test!")
+print("__________________________________")
+client1.send("test!")   # broadcast message to other clients
 client2.send("test@@")
 
-time.sleep(0.4)
 
-print(client3.recive())
+time.sleep(0.5) # some delay in communication
+
+# messages are stored in list
+# calling client.recive() will sent list of messages to output and free stored messages
+list = []
+list.append("test!")
+
+if list == client2.recive():
+    print("List only contain messages from other clients\n")
+
+
+list.append("test@@")
+
+if list == client3.recive():
+    print("Client.recive contain all unread brodcasted messages...")
+    print("succes")
